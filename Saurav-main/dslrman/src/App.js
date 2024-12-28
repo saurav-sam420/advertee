b5a8813f9ae6726e2fad05fc94994786374a6f46
saurav-sam420 <advertee.in@gmail.com>
2024-12-28 14:21:09 +0530

    Delete Saurav-main directory

--- Saurav-main/dslrman/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import AppRoutes from "./routes/approutes";
-import "./App.css"; // Global styles (optional)
-
-function App() {
-  return <AppRoutes />;
-}
-
-export default App;
